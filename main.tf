352035b696a3532cbae2913aaac3380cff1abb2f
hussein13alattas-cyber <hussein.13alattas@gmail.com>
2026-04-16 20:55:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "app_service" {
   app_service_plan_id = module.service_plan.app_service_plan.id
   app_settings = {}
   identity_client_id = "82e6e687-206b-46b8-9301-2d09fd20816a"
-  identity_id = "82e6e687-206b-46b8-9301-2d09fd20816a"
+  identity_id = "/subscriptions/4c569ea4-8bfc-4063-9557-390e4b28a153/resourcegroups/rg-user5/providers/Microsoft.ManagedIdentity/userAssignedIdentities/managedIdentity"
   resource_group = {
     location = "PolandCentral"
     name     = "rg-user5"
